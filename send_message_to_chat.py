53338da53ce41fa1589caf4d9883a5f239e61424
anton <a.solovjev@vzv.su>
2024-02-22 11:31:43 +0300

    Add check for broken token

--- send_message_to_chat.py
+++ send_message_to_chat.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import logging
 
 from environs import Env
@@ -13,14 +14,18 @@ async def send_message(host, port, message, token, buffer_size=1024):
     writer.write(f'{token}\n'.encode())
     await writer.drain()
 
-    logger.info(await reader.read(buffer_size))
-    writer.write(f'\n'.encode())
-    await writer.drain()
+    check_token = await reader.readline()
+    if json.loads(check_token) is None:
+        logger.info('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
+    else:
+        logger.info(await reader.read(buffer_size))
+        writer.write(f'\n'.encode())
+        await writer.drain()
 
-    logger.info(await reader.read(buffer_size))
+        logger.info(await reader.read(buffer_size))
 
-    writer.write(f'{message}\n\n'.encode())
-    await writer.drain()
+        writer.write(f'{message}\n\n'.encode())
+        await writer.drain()
 
     writer.close()
     await writer.wait_closed()
