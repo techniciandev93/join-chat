00de3b9de452b646c02d7996b2074ae2cc01892f
anton <a.solovjev@vzv.su>
2024-02-22 11:02:34 +0300

    Add new script to send messages

--- send_message_to_chat.py
+++ send_message_to_chat.py
@@ -1,23 +1,21 @@
 import asyncio
 
+from environs import Env
 
-async def send_message(host, port, message, token, nic_name, buffer_size=1024):
+
+async def send_message(host, port, message, token, buffer_size=1024):
     reader, writer = await asyncio.open_connection(host, port)
     print(await reader.read(buffer_size))
     writer.write(f'{token}\n'.encode())
     await writer.drain()
 
     print(await reader.read(buffer_size))
-    writer.write(nic_name.encode() + b'\n')
-    await writer.drain()
-
-    print(await reader.read(buffer_size))
-    writer.write(b'\n')
+    writer.write(f'\n'.encode())
     await writer.drain()
 
     print(await reader.read(buffer_size))
 
-    writer.write(message.encode() + b'\n\n')
+    writer.write(f'{message}\n\n'.encode())
     await writer.drain()
 
     writer.close()
@@ -25,9 +23,12 @@ async def send_message(host, port, message, token, nic_name, buffer_size=1024):
 
 
 if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+
+    token = env.str('CHAT_TOKEN')
+
     devman_chat_host = 'minechat.dvmn.org'
     devman_chat_port = 5050
     chat_message = 'hello world'
-    name = 'Anon'
-    token = ''
-    asyncio.run(send_message(devman_chat_host, devman_chat_port, chat_message, token, name))
+    asyncio.run(send_message(devman_chat_host, devman_chat_port, chat_message, token))
